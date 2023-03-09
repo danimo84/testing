d961f9c114e3b6034ba438571b8ff1e3255100c3
danimo84 <moraledadm14@gmail.com>
2023-03-09 21:45:22 +0100

    Test project added

---
+++ pruebas/pruebasApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebasApp.swift
+//  pruebas
+//
+//  Created by Daniel Moraleda on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct pruebasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
